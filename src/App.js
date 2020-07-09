9ba0f009beb6f3080897be43e979596a41b2b172
Sam Vaden <vadensam@yahoo.com>
2020-07-09 12:29:21 -0700

    added state

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div>
     <FirstComponent />
-    <PersonCard fname={"Diane"} lname={"Soyer"} age={"98"} hair={"grey"}/>
-    <PersonCard fname={"Carlos"} lname={"Montoya"} age={"39"} hair={"brown"}/>
+    <PersonCard fname={"Diane"} lname={"Soyer"} age={98} hair={"grey"}/>
+    <PersonCard fname={"Carlos"} lname={"Montoya"} age={39} hair={"brown"}/>
     </div>
   );
 }
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+        fname : this.props.fname,
+        lname : this.props.lname,
+        age : this.props.age,
+        hair : this.props.hair
+        }
+    }
     render(){
-        const { fname, lname, age, hair } = this.props;
+        const { fname, lname, age, hair } = this.state;
         return(
             <div>
                 <h1>{lname}, {fname}</h1>
                 <p>Age: {age}</p>
                 <p>Hair: {hair}</p>
+                <button onClick={ () => { this.setState({age : age+1}) } }>It's My birthday!</button>
             </div>
         )
     }
